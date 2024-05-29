a7e6a7957e2685b09a389c7055d58161496c6dcc
Max Kim <maxkim7100@gmail.com>
2024-05-29 01:18:52 -0400

    Object detection using YOLO V4 tiny

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ def compute_discounted_rewards(rewards, gamma):
 
 
 def update_policy(policy_net, states, actions, rewards, gamma=0.99):
-
+    states = np.array(states)
     states = torch.tensor(states, dtype=torch.float32)
     actions = torch.tensor(actions, dtype=torch.int64).unsqueeze(1)
     rewards = torch.tensor(rewards, dtype=torch.float32)
@@ -84,26 +84,27 @@ with mss.mss() as sct:
 
     retry = False
     new_attempt = False
+    elapsed_time = 0
+    count = 0
 
     while "Screen capturing":
-        last_time = time()
-
         # get raw pixels from the screen, save it to a np array
         img = np.array(sct.grab(monitor))
 
         # region of interest for attempt detection
-        img_cropped = img[100:250, 1:800]
+        img_cropped = img[100:250, 0:1000]
 
         # region of interest of retry button
-        img_retry = img[1:1200, 180:320]
-        hsv = cv.cvtColor(img_retry, cv.COLOR_BGR2HSV)
-        lower_green = np.array([35, 100, 100])
-        upper_green = np.array([85, 255, 255])
-        mask = cv.inRange(hsv, lower_green, upper_green)
-        green_pixels = cv.countNonZero(mask)
+        # img_retry = img[1:1200, 180:320]
+        # hsv = cv.cvtColor(img_retry, cv.COLOR_BGR2HSV)
+        # lower_green = np.array([35, 100, 100])
+        # upper_green = np.array([110, 255, 255])
+        # mask = cv.inRange(hsv, lower_green, upper_green)
+        # green_pixels = cv.countNonZero(mask)
 
         # img_retry_gray = cv.cvtColor(img_retry, cv.COLOR_BGR2GRAY)
-        # _, img_retry_proc = cv.threshold(img_retry_gray, 128, 255, cv.THRESH_BINARY)
+        # _, img_retry_proc = cv.threshold(img_retry_gray, 150, 255, cv.THRESH_BINARY)
+        # black_pixels = cv.countNonZero(img_retry_proc)
 
         # ROI for input to neural net 
         img_nn = img[220:1024, 312:900]
@@ -118,65 +119,93 @@ with mss.mss() as sct:
         # template matching for new attempt detection
         template_cropped = cv.resize(template, (img_cropped.shape[1], img_cropped.shape[0])) # template needs to have matching dimensions as image
         res = cv.matchTemplate(img_proc, template_cropped, cv.TM_CCOEFF_NORMED)
-        threshold = 0.22
+        threshold = 0.20
         loc = np.where(res >= threshold)
 
         # retry_templated_cropped = cv.resize(retry_template, (img_retry.shape[1], img_retry.shape[0])) # template needs to have matching dimensions as image
         # retry_res = cv.matchTemplate(img_retry_proc, retry_templated_cropped, cv.TM_CCOEFF_NORMED)
         # retry_threshold = 0.16
         # retry_loc = np.where(retry_res >= retry_threshold)
-
-        # print(res)
-
-        if green_pixels <= 7000:
-            retry = True
+        # if green_pixels < 3000:
+        #     retry = True
 
         if len(loc[0]) == 1:
+            # print('new')
             new_attempt = True
 
+        if new_attempt and len(loc[0]) == 1:
+            count += 1
+
+        print(count)
+        if count >= 15:
+            new_attempt = False
+            retry = True
+
+        
         # while new_attempt:
         if new_attempt:
-            img = np.array(sct.grab(monitor))
 
+            # last_time = time()
+
+            # elapsed_time += time() - last_time
             # print('attempting')
+            # if elapsed_time > 0.016:
+            #     elapsed_time = 0                
+                
+                
             action, logits = select_action(img_nn_proc, policy_net)
- 
+
             attempt_states.append(img_nn_proc)
             attempt_actions.append(action)
             attempt_rewards.append(reward)
+            reward += 1.0    
+
 
             if action == 1:
-                pass
-                # keyboard.press(Key.space)
-                # keyboard.release(Key.space)
-
-            reward += 1.0             
-            sleep(0.005)
+                keyboard.press(Key.space)
+                keyboard.release(Key.space)            
 
         if retry:
             if not attempt_states:
                 # print('empty')                                                     
                 pass
             else:
+                # print(count)
+                new_attempt = False
                 retry = False
-                # print('updating')
-                # update_policy(policy_net, attempt_states, attempt_actions, attempt_rewards, gamma=0.99)
-                # 
+                count = 0
+                # sleep(0.25)
 
-                attempt_states = []
-                attempt_actions = []       
-                attempt_rewards = []
-                reward = 0.0
+                if len(attempt_states) > 512:
+                    print('updating')
+                    keyboard.press(Key.esc)
+                    sleep(0.02)
+                    keyboard.release(Key.esc)
+                    update_policy(policy_net, attempt_states, attempt_actions, attempt_rewards, gamma=0.99)
+                    print('updated')  
 
-                # print('updated')
-                # after update
+                    keyboard.press(Key.space)
+                    sleep(0.02)
+                    keyboard.release(Key.space) 
+
+                    attempt_states = []
+                    attempt_actions = []       
+                    attempt_rewards = []
+
+                reward = 0.0      
+
+                print('resetting')
+                # sleep(1)
                 # keyboard.press(Key.space)
+                # sleep(0.002)
                 # keyboard.release(Key.space)
+                # print('reset')    
+
 
 
 
         # show preprocessed image
-        cv.imshow('geodash', hsv)
+        cv.imshow('geodash', img_proc)
 
         # press "q" to quit
         if cv.waitKey(1) & 0xFF == ord("q"):
