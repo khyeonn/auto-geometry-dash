ebd8e489ce107fc248e8a914d984d5597703f73d
Max Kim <maxkim7100@gmail.com>
2024-06-01 17:58:50 -0400

    PPO implementation, udpates to yolov4 weights

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import cv2 as cv
 import numpy as np
-from pynput.keyboard import Key, Controller
+import pynput.mouse as ms
+import pynput.keyboard as kb
 from windowcapture import WindowCapture
 from imageprocessor import ImageProcessor
 import time
+from collections import deque
 
-keyboard = Controller()
+from ppo_torch import Agent
+
+keyboard = kb.Controller()
+mouse = ms.Controller()
 
 window_name = "Geometry Dash"
 cfg_file_name = "yolov4-tiny-custom.cfg"
@@ -14,55 +19,163 @@ weights_file_name = "yolov4-tiny-custom_final.weights"
 wincap = WindowCapture(window_name)
 improc = ImageProcessor(wincap.get_window_size(), cfg_file_name, weights_file_name)
 
-while(True):
-    ss = wincap.get_screenshot()
+def press_space():
+    keyboard.press(kb.Key.space)
+    keyboard.release(kb.Key.space)
 
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
+def press_esc():
+    keyboard.press(kb.Key.esc)
+    keyboard.release(kb.Key.esc)
 
-    player_states = improc.proccess_image(ss)
-    
-    
-    for state in player_states:
-        status = np.array([state["class_name"]])
+def retry():
+    mouse.position = (1600, 800)
+    mouse.press(ms.Button.left)
+    mouse.release(ms.Button.left)
 
-        # if status == 'alive':
-        #     keyboard.press(Key.space)
-        #     keyboard.release(Key.space)
+def preprocess(state, game_time):
+    # width = img.shape[1]
+    # height = img.shape[0]
+    # dim = (abs(width/2), abs(height/2))
+    resized = cv.resize(state, (80, 105)) #interpolation = cv2.INTER_AREA)
+    resized = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
+    resized = resized/255.0 # convert all pixel values in [0,1] range
+
+    max_game_time = 2500.0
+    normalized_game_time = game_time/max_game_time
+
+    game_time_channel = np.full((resized.shape[0], resized.shape[1], 1), normalized_game_time)
+
+    state_with_time = np.dstack((resized, game_time_channel))
+
+    # resized = resized.reshape(resized.shape + (1,))
+    return state_with_time
+
+ss = wincap.get_screenshot()
+ss_resized = ss[1:1000, 620:1400]
+input_dims = preprocess(ss_resized, 0).flatten().shape
+
+# countdown 
+for i in range(3, 0, -1):
+    print(i)
+    time.sleep(1)
+retry()
+
+
+## PPO hyperparameters
+buffer_len = 2048
+batch_size = 1024
+n_epochs = 20
+alpha = 1e-3
+policy_clip = 0.2
+agent = Agent(n_actions=2, batch_size=batch_size, buffer_len=buffer_len, policy_clip=policy_clip,
+              alpha=alpha, n_epochs=n_epochs, input_dims=input_dims)
+
+
+score = 0
+old_best_score = 0
+best_score = 0
+avg_score = 0
+score_history = []
+n_attempts = 1
+update_interval = 10
+maxlen = 5
+
+
+# timing
+prev_time = time.time()
+elapsed_time = 0
+total_time = 0
+alive_time = 0
+game_time = 0
+
+done = False
+just_died = True
+buffer = deque(maxlen=maxlen)
+while(total_time < 85):    
+    elapsed_time += time.time() - prev_time
+    total_time += time.time() - prev_time
+    prev_time = time.time()
+    if elapsed_time > 0.016:
+        elapsed_time = 0
+
+        ss = wincap.get_screenshot()
         
-        # else:
-        #     pass
-            
-    
-# with mss.mss() as sct:
-#     # part of the screen to capture
-#     monitor_number = 1
-#     mon = sct.monitors[monitor_number]
-#     monitor = {
-#         "top": mon["top"] + 200,  
-#         "left": mon["left"] + 700, 
-#         "width": 1200,
-#         "height": 1200,
-#         "mon": monitor_number,
-#     }
+        # cv.imshow('a', ss[1:1000, 620:1400])
 
-#     improc = ImageProcessor([1200, 1200], cfg_file_name, weights_file_name)
+        if cv.waitKey(1) == ord('q'):
+            cv.destroyAllWindows()
+            break
 
-#     while "Screen capturing":
-#         # get raw pixels from the screen, save it to a np array
-#         img = np.array(sct.grab(monitor))
-#         img = img[...,:3]
-#         img = np.ascontiguousarray(img) 
+        isAlive = improc.proccess_image(ss)
 
-#         coordinates = improc.proccess_image(img)
-
-#         # cv.imshow('geodash', img)
-
-
-#         if cv.waitKey(1) & 0xFF == ord("q"):
-#                     cv.destroyAllWindows()
-#                     break
-            
-    
+        if isAlive:
+            alive_time += 1 
+        else:
+            alive_time -= 1
         
+        if alive_time > 10:
+            done = False
+            alive_time = 0
+            just_died = False
+        elif alive_time < -10:
+            done = True
+            alive_time = 0
+
+        if not done:
+            reward = 0.1
+            game_time += 1
+
+            observation = preprocess(ss[1:1000, 620:1400], game_time)
+            action, prob, val = agent.choose_action(observation.flatten())
+
+            # # print(action, prob, val)
+            if action == 1:
+                press_space() 
+
+            score += reward
+
+            agent.remember(observation.flatten(), action, prob, val, reward, done)
+            buffer.append((observation, action, prob, val, reward, done, ss))
+        else:
+            if not just_died:
+                just_died = True
+                game_time = 0
+                total_time = 0
+                for _ in range(maxlen):
+                    agent.forget()
+
+                for i in range(maxlen):
+                    last_observation, last_action, last_prob, last_val, _, _, ss = buffer[i]
+                    # cv.imshow('a', ss)
+                    agent.remember(last_observation.flatten(), last_action, last_prob, last_val, 0, True)
+                    
+
+                buffer.clear()
+                score_history.append(score)
+                best_score = np.max(score_history)
+                avg_score = np.mean(score_history[-100:])
+                n_attempts += 1
+                score = 0 
+
+                if best_score > old_best_score:
+                    old_best_score = best_score
+                    agent.save_models()
+
+        if n_attempts % update_interval == 0:
+            n_attempts += 1
+            print('... learning ...')
+            press_esc()
+            agent.learn()
+            retry()
+
+            # reset variables
+            game_time = 0
+            total_time = 0
+            alive_time = 0
+            score = 0
+            buffer.clear()
+            just_died = True
+
+        # if game_time % 10 == 0 and game_time != 0:
+        #     print('attempt', n_attempts, 'score %.1f' % score, 'best score %.1f' % old_best_score, 'avg score %.1f' % avg_score)
+
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from pynput.keyboard import Key, Controller
 from windowcapture import WindowCapture
 from imageprocessor import ImageProcessor
 import time
+import mss
+from ppo_torch import Agent
 
 keyboard = Controller()
 
@@ -14,55 +16,64 @@ weights_file_name = "yolov4-tiny-custom_final.weights"
 wincap = WindowCapture(window_name)
 improc = ImageProcessor(wincap.get_window_size(), cfg_file_name, weights_file_name)
 
-while(True):
-    ss = wincap.get_screenshot()
+# while(True):
+#     ss = wincap.get_screenshot()
 
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
+#     if cv.waitKey(1) == ord('q'):
+#         cv.destroyAllWindows()
+#         break
 
-    player_states = improc.proccess_image(ss)
+#     player_states = improc.proccess_image(ss)
     
     
-    for state in player_states:
-        status = np.array([state["class_name"]])
+#     for state in player_states:
+#         status = np.array([state["class_name"]])
 
-        # if status == 'alive':
-        #     keyboard.press(Key.space)
-        #     keyboard.release(Key.space)
+#         # if status == 'alive':
+#         #     keyboard.press(Key.space)
+#         #     keyboard.release(Key.space)
         
-        # else:
-        #     pass
+#         # else:
+#         #     pass
             
+
     
-# with mss.mss() as sct:
-#     # part of the screen to capture
-#     monitor_number = 1
-#     mon = sct.monitors[monitor_number]
-#     monitor = {
-#         "top": mon["top"] + 200,  
-#         "left": mon["left"] + 700, 
-#         "width": 1200,
-#         "height": 1200,
-#         "mon": monitor_number,
-#     }
+with mss.mss() as sct:
+    # part of the screen to capture
+    monitor_number = 1
+    mon = sct.monitors[monitor_number]
+    monitor = {
+        "top": mon["top"] + 200,  
+        "left": mon["left"] + 700, 
+        "width": 1200,
+        "height": 1200,
+        "mon": monitor_number,
+    }
 
-#     improc = ImageProcessor([1200, 1200], cfg_file_name, weights_file_name)
+    improc = ImageProcessor([1200, 1200], cfg_file_name, weights_file_name)
 
-#     while "Screen capturing":
-#         # get raw pixels from the screen, save it to a np array
-#         img = np.array(sct.grab(monitor))
-#         img = img[...,:3]
-#         img = np.ascontiguousarray(img) 
+    batch_size = 5
+    n_epochs = 4
+    alpha = 0.0003
+    agent = Agent(n_actions=2, batch_size=batch_size,
+                alpha=alpha, n_epochs=n_epochs, input_dims=np.prod((1200, 1200)))
 
-#         coordinates = improc.proccess_image(img)
+    while "Screen capturing":
+        # get raw pixels from the screen, save it to a np array
+        img = np.array(sct.grab(monitor))
+        img = img[...,:3]
+        img = np.ascontiguousarray(img) 
 
-#         # cv.imshow('geodash', img)
+        coordinates = improc.proccess_image(img)
+
+        # cv.imshow('geodash', img)
+
+        action, prob, val = agent.choose_action(img.flatten())
 
 
-#         if cv.waitKey(1) & 0xFF == ord("q"):
-#                     cv.destroyAllWindows()
-#                     break
+        if cv.waitKey(1) & 0xFF == ord("q"):
+                    cv.destroyAllWindows()
+                    break
             
     
         
